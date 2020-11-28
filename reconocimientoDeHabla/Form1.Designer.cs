28bf86947e3d060863b3b7d559a39aaffc24e168
Gustavo Garcia <gustavo-garcia1994@hotmail.com>
2020-11-27 22:19:16 -0500

    primer commit

---
+++ reconocimientoDeHabla/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace reconocimientoDeHabla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDetener = new System.Windows.Forms.Button();
+            this.BtnIniciar = new System.Windows.Forms.Button();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnDetener
+            // 
+            this.BtnDetener.Location = new System.Drawing.Point(233, 74);
+            this.BtnDetener.Name = "BtnDetener";
+            this.BtnDetener.Size = new System.Drawing.Size(75, 23);
+            this.BtnDetener.TabIndex = 0;
+            this.BtnDetener.Text = "Detener";
+            this.BtnDetener.UseVisualStyleBackColor = true;
+            // 
+            // BtnIniciar
+            // 
+            this.BtnIniciar.Location = new System.Drawing.Point(338, 74);
+            this.BtnIniciar.Name = "BtnIniciar";
+            this.BtnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.BtnIniciar.TabIndex = 1;
+            this.BtnIniciar.Text = "Iniciar";
+            this.BtnIniciar.UseVisualStyleBackColor = true;
+            this.BtnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(21, 27);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(393, 20);
+            this.txtTexto.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.BtnIniciar);
+            this.Controls.Add(this.BtnDetener);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDetener;
+        private System.Windows.Forms.Button BtnIniciar;
+        private System.Windows.Forms.TextBox txtTexto;
+    }
+}
+
---
+++ reconocimientoDeHabla/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace reconocimientoDeHabla
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine oEscucha = new SpeechRecognitionEngine();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            oEscucha.SetInputToDefaultAudioDevice();
+            oEscucha
+        }
+    }
+}
